a8611fefe7b14e9088a83f9842c711a4eb0d1714
Marylise Larouche <MALAR563@ulaval.ca>
2026-03-19 13:55:33 -0400

    Figures finies

--- marylise_fluorescence.py
+++ marylise_fluorescence.py
@@ -79,7 +79,7 @@ def pre_process(folder, file_name):
     # plt.plot(energies, counts)
     # plt.show()
 
-    return energies, counts_s
+    return energies, counts_s, time
 
 def sum_up_sigma(energies, counts, mean, std, n=2):
     # intervalle sur l'axe des ÉNERGIES
@@ -91,7 +91,9 @@ def sum_up_sigma(energies, counts, mean, std, n=2):
 
     return np.sum(counts[mask])
 
-
+def calcule_incertitudes_ratio(resultat, valeur1, sigma1, valeur2, sigma2):
+    sigma_resultat = resultat*np.sqrt((sigma1/valeur1)**2 + (sigma2/valeur2)**2)# - 2*((sigma1*valeur2)/(valeur1*valeur2)))
+    return sigma_resultat
 
 
 """ANALYSE DES SPECTRES PURS"""
@@ -107,7 +109,8 @@ for file in files:
 
     index = df.index.to_numpy()
     counts = (df[df.columns[0]]).to_numpy()
-    counts_s = counts/time
+    # counts_s = counts/time
+    counts_s = (df[df.columns[0]]).to_numpy() # iciiii
 
     energies = (0.009685321409623682*index) - 0.059015714868320815
 
@@ -122,58 +125,68 @@ for file in files:
     if element == "Al":
         mu_Al1, mu_err_Al1, sigma_Al1, sigma_err_Al1, _, A = fit_peak(energies, counts_s, peak_center=1.49, window=0.5, plot=False) 
     if element == "Cu":
-        mu_Cu1, mu_err_Cu, sigma_Cu1, sigma_err_Cu1, _, A1 = fit_peak(energies, counts_s, peak_center=8.05, window=0.5, plot=True)
+        time_Cu = time
+        mu_Cu1, mu_err_Cu, sigma_Cu1, sigma_err_Cu1, N_Cu1_ref, A1 = fit_peak(energies, counts_s, peak_center=8.05, window=0.5, plot=True)
         total_counts_REF_Cu1 = sum_up_sigma(energies, gaussian_without_background(energies, A1, mu_Cu1, sigma_Cu1), mu_Cu1, sigma_Cu1) 
-        mu_Cu2, mu_err_Cu2, sigma_Cu2, sigma_err_Cu2, _, A2 = fit_peak(energies, counts_s, peak_center=8.91, window=0.5, plot=True)
+        mu_Cu2, mu_err_Cu2, sigma_Cu2, sigma_err_Cu2, N_Cu2_ref, A2 = fit_peak(energies, counts_s, peak_center=8.91, window=0.5, plot=True)
         total_counts_REF_Cu2 = sum_up_sigma(energies, gaussian_without_background(energies, A2, mu_Cu2, sigma_Cu2), mu_Cu2, sigma_Cu2) 
+    
+        total_counts_REF_Cu2_s = total_counts_REF_Cu2/time_Cu# iciii
+        total_counts_REF_Cu1_s = total_counts_REF_Cu1/time_Cu# iciii
     if element == "Fe":
-        mu_Fe1, mu_err_Fe, sigma_Fe1, sigma_err_Fe1, _, A1 = fit_peak(energies, counts_s, peak_center=6.4, window=0.5, plot=False)
-        total_counts_REF_Fe1 = sum_up_sigma(energies, gaussian_without_background(energies, A1, mu_Fe1, sigma_Fe1), mu_Fe1, sigma_Fe1) 
-        mu_Fe2, mu_err_Fe2, sigma_Fe2, sigma_err_Fe2, _, A2 = fit_peak(energies, counts_s, peak_center=7.06, window=0.3, plot=False)
+        time_Fe = time
+        mu_Fe1, mu_err_Fe, sigma_Fe1, sigma_err_Fe1, N_Fe1_ref, A1 = fit_peak(energies, counts_s, peak_center=6.4, window=0.5, plot=False)
+        total_counts_REF_Fe1 = sum_up_sigma(energies, gaussian_without_background(energies, A1, mu_Fe1, sigma_Fe1), mu_Fe1, sigma_Fe1)
+        mu_Fe2, mu_err_Fe2, sigma_Fe2, sigma_err_Fe2, N_Fe2_ref, A2 = fit_peak(energies, counts_s, peak_center=7.06, window=0.3, plot=False)
         total_counts_REF_Fe2 = sum_up_sigma(energies, gaussian_without_background(energies, A2, mu_Fe2, sigma_Fe2), mu_Fe2, sigma_Fe2)
+
+        total_counts_REF_Fe2_s = total_counts_REF_Fe2/time_Fe # iciii
+        total_counts_REF_Fe1_s = total_counts_REF_Fe1/time_Fe# iciii
     if element == "Pb":
         mu_Pb1, mu_err_Pb, sigma_Pb1, sigma_err_Pb1, _, A = fit_peak(energies, counts_s, peak_center=9.18, window=0.5, plot=False) 
         mu_Pb2, mu_err_Pb2, sigma_Pb2, sigma_err_Pb2, _, A = fit_peak(energies, counts_s, peak_center=10.55, window=0.5, plot=False)
         mu_Pb3, mu_err_Pb3, sigma_Pb3, sigma_err_Pb3, _, A = fit_peak(energies, counts_s, peak_center=12.61, window=0.5, plot=False)
         mu_Pb4, mu_err_Pb4, sigma_Pb4, sigma_err_Pb4, _, A = fit_peak(energies, counts_s, peak_center=14.76, window=0.5, plot=False)
 
+# ratio_ref_Fe1_Cu1 = total_counts_REF_Fe1/total_counts_REF_Cu1
+# ratio_ref_Fe1_Cu2 = total_counts_REF_Fe1/total_counts_REF_Cu2
+ratio_ref_Fe1_Cu1 = total_counts_REF_Fe1_s/total_counts_REF_Cu1_s # iciii
+ratio_ref_Fe1_Cu2 = total_counts_REF_Fe1_s/total_counts_REF_Cu2_s # iciii
+# ratio_ref_Fe1_Cu1 = N_Fe1_ref/N_Cu1_ref
+# ratio_ref_Fe1_Cu2 = N_Fe1_ref/N_Cu2_ref
+# i_ratio_ref_Fe1_Cu1 = calcule_incertitudes_ratio(ratio_ref_Fe1_Cu1, total_counts_REF_Fe1, np.sqrt(total_counts_REF_Fe1), total_counts_REF_Cu1, np.sqrt(total_counts_REF_Cu1))
+# i_ratio_ref_Fe1_Cu2 = calcule_incertitudes_ratio(ratio_ref_Fe1_Cu2, total_counts_REF_Fe1, np.sqrt(total_counts_REF_Fe1), total_counts_REF_Cu2, np.sqrt(total_counts_REF_Cu2))
+i_ratio_ref_Fe1_Cu1 = calcule_incertitudes_ratio(ratio_ref_Fe1_Cu1, total_counts_REF_Fe1_s, np.sqrt(total_counts_REF_Fe1)/time_Fe, total_counts_REF_Cu1_s, np.sqrt(total_counts_REF_Cu1)/time_Cu) #iciii
+i_ratio_ref_Fe1_Cu2 = calcule_incertitudes_ratio(ratio_ref_Fe1_Cu2, total_counts_REF_Fe1_s, np.sqrt(total_counts_REF_Fe1)/time_Fe, total_counts_REF_Cu2_s, np.sqrt(total_counts_REF_Cu2)/time_Cu) #iciii
+
+
+
+"""SECTION RÉSOLUTION"""
 def resolution_theorique(energies):
     return (((260**2) - (120**2) + 2440*energies)**(1/2))/1000
 
 def racine_carre(energies, A, k):
     return ((k + (A*energies))**(1/2))
 
-
 resolutions = 2.35482*np.array([sigma_Ag1, sigma_Ag2, sigma_Cu1, sigma_Cu2, sigma_Fe1, sigma_Fe2, sigma_Pb1, sigma_Pb1, sigma_Pb3, sigma_Pb4])
+i_resolutions = 2.35482*np.array([sigma_err_Ag1, sigma_err_Ag2, sigma_err_Cu1, sigma_err_Cu2, sigma_err_Fe1, sigma_err_Fe2, sigma_err_Pb1, sigma_err_Pb1, sigma_err_Pb3, sigma_err_Pb4])
 energies_pics = [22.16, 24.94, 8.05, 8.91, 6.4, 7.06, 9.18, 10.55, 12.61, 14.76]
 
-print(resolutions)
-
-# 1. On "lie" les deux listes ensemble
-couples = zip(energies_pics, resolutions)
-# 2. On trie les couples (par défaut, sorted trie selon le premier élément : l'énergie)
-couples_tries = sorted(couples)
-# 3. On "délie" les couples pour récupérer deux listes distinctes triées
-energies_pics_triees, resolutions_triees = zip(*couples_tries)
-# Optionnel : Re-convertir en array ou liste si nécessaire
-energies_pics = list(energies_pics_triees)
-resolutions = np.array(resolutions_triees)
-
-popt, pcov = curve_fit(racine_carre, energies_pics, resolutions, p0=[0.0012, 0.01]) #(2440/(1000**0.5)), (((260**2)-(120**2))/(1000**0.5))
-
-print(popt, pcov)
+popt, pcov = curve_fit(racine_carre, energies_pics, resolutions, p0=[0.0012, 0.01], sigma=i_resolutions) #(2440/(1000**0.5)), (((260**2)-(120**2))/(1000**0.5))
 i_fit = np.diag(pcov)**0.5
 
-plt.plot(energies_pics, resolutions, "ro")
+# plt.plot(energies_pics, resolutions, "ro")
+plt.errorbar(energies_pics, resolutions, yerr=i_resolutions, fmt='ro', ecolor = 'black',capsize=5, label = 'Énergies maximales')
 plt.plot(energies, racine_carre(energies, popt[0], popt[1]), color="red", linestyle="--", label="Fonction ajustée")
-plt.plot(energies, resolution_theorique(energies), "black", label="Résolution théorique")
+plt.plot(energies, resolution_theorique(energies), "blue", label="Résolution théorique")
 plt.fill_between(energies, racine_carre(energies, popt[0]-i_fit[0], popt[1]-i_fit[1]), racine_carre(energies, popt[0]+i_fit[0], popt[1]+i_fit[1]), alpha=0.2, color="red")
-plt.text(10, 38, r"$FWHM_{théor.}$"+ f" = {popt[0]:.2f} kV {popt[1]:.2f}", fontsize=12)#
-plt.text(30, 10, r"$FWHM_{fit}$"+ f' = {popt_moy[0]:.2f} kV +{popt_moy[1]:.2f}', fontsize=12)#
+plt.text(15, 0.25, fr"$FWHM_{{théor.}} = \frac{{\sqrt{{53200 + 2440 E}}}}{{1000}}$", fontsize=12)#
+plt.text(0, 0.45, fr"$FWHM_{{fit}} = \sqrt{{{popt[1]:.3g} + {popt[0]:.3g} E}}$", fontsize=12)#
 plt.xlabel("Énergies [keV]")
 plt.ylabel("Résolution (FWHM) [keV]")
 plt.legend()
 plt.show()
+
 
 
 
@@ -184,40 +197,68 @@ folder = f"spectres_bruts/fluorescence_csv/alliages"
 
 files = os.listdir(folder)
 for file in files:
-    energies, counts = pre_process(folder, file)
+    print(file)
+    energies, counts, time = pre_process(folder, file)
     try:    
-        mu_Cu1, mu_err_Cu, sigma_Cu1, sigma_err_Cu1, _, A1 = fit_peak(energies, counts, peak_center=8.05, window=0.4)
+        # mu_Cu1, mu_err_Cu, sigma_Cu1, sigma_err_Cu1, N_Cu1, A1 = fit_peak(energies, counts, peak_center=8.05, window=0.4)
+        mu_Cu1, mu_err_Cu, sigma_Cu1, sigma_err_Cu1, N_Cu1, A1 = fit_peak(energies, time*counts, peak_center=8.05, window=0.4)#iciii
         plt.plot(energies, gaussian_without_background(energies, A1, mu_Cu1, sigma_Cu1))
         plt.plot(energies, counts)
         plt.show()
         total_counts_Cu1 = sum_up_sigma(energies, gaussian_without_background(energies, A1, mu_Cu1, sigma_Cu1), mu_Cu1, sigma_Cu1)
-        ratio_Cu1 = total_counts_Cu1/total_counts_REF_Cu1
-        print(total_counts_Cu1, total_counts_REF_Cu1)
-        print(ratio_Cu1)
+        total_counts_Cu1_s = sum_up_sigma(energies, gaussian_without_background(energies, A1, mu_Cu1, sigma_Cu1), mu_Cu1, sigma_Cu1)/time #iciii
 
-        mu_Cu2, mu_err_Cu2, sigma_Cu2, sigma_err_Cu2, _, A2 = fit_peak(energies, counts, peak_center=8.91, window=0.3)#0.5 pour cle
+  
+
+        # mu_Cu2, mu_err_Cu2, sigma_Cu2, sigma_err_Cu2, N_Cu2, A2 = fit_peak(energies, counts, peak_center=8.91, window=0.3)#0.5 pour cle
+        mu_Cu2, mu_err_Cu2, sigma_Cu2, sigma_err_Cu2, N_Cu2, A2 = fit_peak(energies, time*counts, peak_center=8.91, window=0.3)#0.5 pour cle # iciiii
         plt.plot(energies, gaussian_without_background(energies, A2, mu_Cu2, sigma_Cu2))
         plt.plot(energies, counts)
         plt.show()
         total_counts_Cu2 = sum_up_sigma(energies, gaussian_without_background(energies, A2, mu_Cu2, sigma_Cu2), mu_Cu2, sigma_Cu2)
-        ratio_Cu2 = total_counts_Cu2/total_counts_REF_Cu2
-        print(ratio_Cu2)
+        total_counts_Cu2_s = sum_up_sigma(energies, gaussian_without_background(energies, A2, mu_Cu2, sigma_Cu2), mu_Cu2, sigma_Cu2)/time #iciiii
+        # ratio_Cu2 = total_counts_Cu2/total_counts_REF_Cu2
+        # print(ratio_Cu2)
 
-        mu_Fe1, mu_err_Fe, sigma_Fe1, sigma_err_Fe1, _, A3 = fit_peak(energies, counts, peak_center=6.4, window=0.5)
+        # mu_Fe1, mu_err_Fe, sigma_Fe1, sigma_err_Fe1, N_Fe1, A3 = fit_peak(energies, counts, peak_center=6.4, window=0.5)
+        mu_Fe1, mu_err_Fe, sigma_Fe1, sigma_err_Fe1, N_Fe1, A3 = fit_peak(energies, time*counts, peak_center=6.4, window=0.5) #iciiii
         plt.plot(energies, gaussian_without_background(energies, A3, mu_Fe1, sigma_Fe1))
         plt.plot(energies, counts)
         plt.show()
         total_counts_Fe1 = sum_up_sigma(energies, gaussian_without_background(energies, A3, mu_Fe1, sigma_Fe1), mu_Fe1, sigma_Fe1)
-        ratio_Fe1 = total_counts_Fe1/total_counts_REF_Fe1
-        print(ratio_Fe1)
+        total_counts_Fe1_s = sum_up_sigma(energies, gaussian_without_background(energies, A3, mu_Fe1, sigma_Fe1), mu_Fe1, sigma_Fe1)/time #iciii
+        # ratio_Fe1 = total_counts_Fe1/total_counts_REF_Fe1
+        # print(ratio_Fe1)
+
+        ratio_Fe1_Cu1 = total_counts_Fe1/total_counts_Cu1
+        ratio_Fe1_Cu2 = total_counts_Fe1/total_counts_Cu2
+        ratio_Fe1_Cu1 = total_counts_Fe1_s/total_counts_Cu1_s # iciii
+        ratio_Fe1_Cu2 = total_counts_Fe1_s/total_counts_Cu2_s # iciii
+        # ratio_Fe1_Cu1 = N_Fe1/N_Cu1
+        # ratio_Fe1_Cu2 = N_Fe1/N_Cu2
+        i_ratio_Fe1_Cu1 = calcule_incertitudes_ratio(ratio_Fe1_Cu1, total_counts_Fe1, np.sqrt(total_counts_Fe1), total_counts_Cu1, np.sqrt(total_counts_Cu1))
+        i_ratio_Fe1_Cu2 = calcule_incertitudes_ratio(ratio_Fe1_Cu2, total_counts_Fe1, np.sqrt(total_counts_Fe1), total_counts_Cu2, np.sqrt(total_counts_Cu2))
+        i_ratio_Fe1_Cu1 = calcule_incertitudes_ratio(ratio_Fe1_Cu1, total_counts_Fe1_s, np.sqrt(total_counts_Fe1)/time, total_counts_Cu1_s, np.sqrt(total_counts_Cu1)/time) #iciii
+        i_ratio_Fe1_Cu2 = calcule_incertitudes_ratio(ratio_Fe1_Cu2, total_counts_Fe1_s, np.sqrt(total_counts_Fe1)/time, total_counts_Cu2_s, np.sqrt(total_counts_Cu2)/time) #iciii
+
+
+        intensite_rel_1 = ratio_Fe1_Cu1/ratio_ref_Fe1_Cu1
+        intensite_rel_2 = ratio_Fe1_Cu2/ratio_ref_Fe1_Cu2
+        i_intensite_rel_1 = calcule_incertitudes_ratio(intensite_rel_1, ratio_Fe1_Cu1, i_ratio_Fe1_Cu1, ratio_ref_Fe1_Cu1, ratio_ref_Fe1_Cu1)
+        i_intensite_rel_2 = calcule_incertitudes_ratio(intensite_rel_2, ratio_Fe1_Cu2, i_ratio_Fe1_Cu2, ratio_ref_Fe1_Cu2, i_ratio_ref_Fe1_Cu2)
+        
+        print(intensite_rel_1, i_intensite_rel_1)
+        print(intensite_rel_2, i_intensite_rel_2)
+        
+
 
         mu_Fe2, mu_err_Fe2, sigma_Fe2, sigma_err_Fe2, _, A4 = fit_peak(energies, counts, peak_center=7.06, window=0.3)
         plt.plot(energies, gaussian_without_background(energies, A4, mu_Fe2, sigma_Fe2))
         plt.plot(energies, counts)
         plt.show()
         total_counts_Fe2 = sum_up_sigma(energies, gaussian_without_background(energies, A4, mu_Fe2, sigma_Fe2), mu_Fe2, sigma_Fe2)
-        ratio_Fe2 = total_counts_Fe2/total_counts_REF_Fe2
-        print(ratio_Fe2)
+        # ratio_Fe2 = total_counts_Fe2/total_counts_REF_Fe2
+        # print(ratio_Fe2)
 
     except:
         pass
